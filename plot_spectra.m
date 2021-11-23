15f28fd6c3708662156e6bb168ef64bbc76632d2
Federico Rapisarda <70779276+Federicorapi@users.noreply.github.com>
2021-11-23 13:59:21 +0100

    Add files via upload

---
+++ plot_spectra.m
@@ -0,0 +1,20 @@
+function [] = plot_spectra (x,y,name)
+
+data = h5read(name,'/Data');
+wvl = h5read(name,'/WL');
+xy = h5read(name,'/Consigne');
+
+
+spectra= data(:,x+(y-1)*21);
+
+
+figure()
+plot(wvl(226:1340),spectra(226:1340))
+xlabel('Wavelength, nm')
+ylabel('Counts')
+titre=strcat('Spectra at pixel',' (',num2str(x),',',num2str(y),')');
+subtitle('Puissance 67µW, 3s exposure time, 632.8 nm excitation')
+title(titre)
+
+
+
