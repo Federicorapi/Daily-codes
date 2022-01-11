6cd82a5334d9ee2a3eaffd86e349240d8a715089
Federico Rapisarda <70779276+Federicorapi@users.noreply.github.com>
2022-01-11 09:54:17 +0100

    Update scan_h5.m

--- scan_h5.m
+++ scan_h5.m
@@ -2,14 +2,17 @@ clear all
 clc
 close all
 %% Opening of h5 file
-name = 'LASER_ML_on_SiO2_20�W_0.004exposure_spectro632_excitation_632nm_AVEC_WINDOW_FOCALISE_001.h5';      %% Insert file name
+[File, Path] = uigetfile('*.h5');   %% Insert file name
+%I_Ml_on_SiO2_0.3xname = 'PL_PAS_FIN0.7V_resolution_0.005V_0.4s_exposure_spectro750_excitation632_20µW_Olympus_002.h5_001.h5';      %% Insert file name
+name = strcat(Path,File);
 data = h5read(name,'/Data');
 wvl = h5read(name,'/WL');
 ev=1240/wvl;
 xy = h5read(name,'/Consigne');
 pixel=length(wvl);
-scansizex=81;       %% Insert number of steps along x
-scansizey=51;       %% Insert number of steps along y
+step_volt = xy(1,2)-xy(1,1);
+scansizex=int64((xy(1,end)-xy(1,1))/step_volt+1);       %% Insert number of steps along x
+scansizey=int64((xy(2,end)-xy(2,1))/step_volt+1);      %% Insert number of steps along y
 
 %% Creation of one spectrum to see the background
 sum_of_spectra = zeros(pixel,1);
@@ -19,7 +22,7 @@ for ii = 1:length(xy(1,:))
 end
 figure(1),plot(wvl,sum_of_spectra);
 title('Sum of all the spectra')
-xlabel('Wavelength, nm')
+xlabel('Wavelength, nm') 
 ylabel('A.U.')
 
 
@@ -29,17 +32,18 @@ background = zeros(scansizex,scansizey);
 r = 1;
 for y = 1:scansizey
     for x = 1:scansizex
-        intensities (x,y) = max(data(1:pixel,x+(r-1)*scansizex));
+        intensities(x,y) = max(data(1:pixel,x+(r-1)*scansizex));
         intensitiestrapz(x,y) = trapz(data(1:pixel,x+(r-1)*scansizex));
         %background (x,y) = mean(data(1:17,x+(r-1)*S));
     end
     r = r+1;
 end
-figure(4)
-imagesc(intensities)
+% figure(4)
+% imagesc(intensities)
+% colorbar
 %% Muon killing
 
-treshold=3000;
+treshold=1300;
 for ss=1:scansizey
     for kk=1:scansizex
         if max(intensities(kk,ss))>treshold
@@ -54,10 +58,12 @@ for ss=1:scansizey
         end
     end
 end
+% intensities(1,end) = intensities(2,end);
 
-figure(5)
-imagesc(intensities)
-title('Map with muons killed')
+% figure(5)
+% imagesc(intensities)
+% title('Map with muons killed')
+% colorbar
 
 
 %% Normalisation
@@ -68,94 +74,93 @@ title('Map with muons killed')
 % data=data./normfactor;
 % data=data./max(max(data));
 
-%% Plot of a slice
-
-tranche=zeros(1,scansizex);
-yline =5;
-xline =0;
-
-for jj=1:scansizex
-    tranche(jj) = intensities(jj,yline);
-end
-
-% for jj=1:scansizey
-%     tranche(jj) = intensities(xline,jj);
+% %% Plot of a slice
+% 
+% tranche=zeros(1,scansizex);
+% yline =5;
+% xline =0;
+% 
+% for jj=1:scansizex
+%     tranche(jj) = intensities(jj,yline);
 % end
-
-
-%m=mean(tranche(40:81));
-m=min(tranche);
-%tranche=(tranche-m);
-%M=mean(tranche(1:28));
-M=max(tranche);
-%tranche=tranche./M;
-
-
-% tranche(40:81)=m;
-% tranche=(tranche-m);
-% tranche(1:28)=M;
-% tranche=tranche./M;
-lignex=2*2.64*3*linspace(1,scansizex,scansizex);
-colonney=0.7*2*2.64*3*linspace(1,scansizey,scansizey);
-
-
-%tranchecut=tranche(1:20);
-%lignexcut=lignex(1:20);
-
-figure(6)
-% plot(colonney,tranche)
- plot(lignex,tranche)
-%plot(lignexcut,tranchecut)
-
-%c*(1/2)*(1+erf((x-a)/(sqrt(2)*b)))
-
-% load('fitnowindow.mat')
-% load('fitwithwindow.mat')
-% figure()
-% plot(fitnowindow)
-% title('Fit no window, on ML')
-% xticks('auto')
-% yticks('auto')
-% xlabel('Microns')
-% ylabel('Microns')
-% figure()
-% plot(fitwithwindow,'b')
-% title('Fit with window, on ML')
-% xticks('auto')
-% yticks('auto')
-% xlabel('Microns')
-% ylabel('Microns')
-
+% 
+% % for jj=1:scansizey
+% %     tranche(jj) = intensities(xline,jj);
+% % end
+% 
+% 
+% %m=mean(tranche(40:81));
+% m=min(tranche);
+% %tranche=(tranche-m);
+% %M=mean(tranche(1:28));
+% M=max(tranche);
+% %tranche=tranche./M;
+% 
+% 
+% % tranche(40:81)=m;
+% % tranche=(tranche-m);
+% % tranche(1:28)=M;
+% % tranche=tranche./M;
+% lignex=2*2.64*3*linspace(1,scansizex,scansizex);
+% colonney=0.7*2*2.64*3*linspace(1,scansizey,scansizey);
+% 
+% 
+% %tranchecut=tranche(1:20);
+% %lignexcut=lignex(1:20);
+% 
+% figure(6)
+% % plot(colonney,tranche)
+%  plot(lignex,tranche)
+% %plot(lignexcut,tranchecut)
+% 
+% %c*(1/2)*(1+erf((x-a)/(sqrt(2)*b)))
+% 
+% % load('fitnowindow.mat')
+% % load('fitwithwindow.mat')
+% % figure()
+% % plot(fitnowindow)
+% % title('Fit no window, on ML')
+% % xticks('auto')
+% % yticks('auto')
+% % xlabel('Microns')
+% % ylabel('Microns')
+% % figure()
+% % plot(fitwithwindow,'b')
+% % title('Fit with window, on ML')
+% % xticks('auto')
+% % yticks('auto')
+% % xlabel('Microns')
+% % ylabel('Microns')
+% 
 %% Rescaling of the axis
-
-xy(1,:)=2*2.64*3*xy(1,:);
-xy(2,:)=0.7*2*2.64*3*xy(2,:);
+objective = 1; %Put 1 for Mitutotyo, 2 for Olympus
+xy(1,:)=objective*2*2.64*3*xy(1,:); %from volt to micron  %MULTIPLY BOTH BY 2 TO USE THE OLYMPUS
+xy(2,:)=objective*0.7*2*2.64*3*xy(2,:);   %from volt to micron
 
 % Plot of the max intensity map
 figure(7),
-imagesc(transpose(intensities))
-%imagesc(xy(1,:),xy(2,:),transpose(intensities))
+% imagesc(transpose(intensities))
+intensities_transpose = transpose(intensities);
+imagesc(xy(1,:),xy(2,:),intensities_transpose)
 axis xy
 axis equal
 xticks('auto')
 yticks('auto')
-xlabel('pixels')
-ylabel('pixels')
+xlabel('Micron')
+ylabel('Micron')
 
 colorbar
-title('Laser scan of ML on SiO2. 20 \muW, 0.004 seconds. Olympus objective')
-ylim([1 scansizey-1])
+titolo = strcat('Scan 0.4 seconds, 20\mu W excitation,',num2str(step_volt),'V steps');
+title(titolo)
+ylim([xy(2,1) xy(2,end)])
+xlim([xy(1,1) xy(1,end)])
+figure(8), imagesc(intensities_transpose)
+title('Image with pixel reference')
+%% Evaluation of resolution
 
-%% Region of interest on the map
-%% Select the pixel interval to create the ROI
-limitx1=41;
-limitx2=46;
-limity1=3;
-limity2=8;
-spectraROI= 0;
-
-    for x = limitx1:limitx2
-        for y = limity1:limity2
-            spectraROI=spectraROI+intensitiestrapz(x,y);
-        end
-    end
+tranche = 38; % Inserire qui il numero di riga su cui tagliare
+xdirection = [1:30];
+cut = intensities_transpose(tranche,xdirection);
+cut = cut-min(cut);
+cut = cut./max(cut);
+figure(9), plot(xdirection,cut)
